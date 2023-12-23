8478acc21e841a6332a380825429b619e79a61d6
mao <1296193245@qq.com>
2023-12-23 21:08:05 +0800

    uniapp-location

---
+++ pages/index/test1/test1.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<text>
+			<h1>{{longitude}}</h1>
+			<h1>{{latitude}}</h1>
+		</text>
+		<button type="primary" @click="get">获取地址</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				longitude: 0,
+				latitude: 0,
+			}
+		},
+		methods: {
+			get() {
+				uni.getLocation({
+					success: (res) => {
+						console.log("返回数据：",res);
+						console.log('当前位置的经度：' + res.longitude);
+						console.log('当前位置的纬度：' + res.latitude);
+						this.longitude = res.longitude
+						this.latitude = res.latitude
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/test2/test2.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<text>
+			<h1>{{longitude}}</h1>
+			<h1>{{latitude}}</h1>
+		</text>
+		<button type="primary" @click="get">选择城市</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				longitude: 0,
+				latitude: 0,
+			}
+		},
+		methods: {
+			get() {
+				uni.chooseLocation({
+					success: (res) => {
+						console.log(res);
+						console.log('位置名称：' + res.name);
+						console.log('详细地址：' + res.address);
+						console.log('纬度：' + res.latitude);
+						console.log('经度：' + res.longitude);
+						this.longitude = res.longitude
+						this.latitude = res.latitude
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
