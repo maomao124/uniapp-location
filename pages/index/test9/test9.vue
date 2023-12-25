115f2db98d727afbcf09d10347bd52091df498c9
mao <1296193245@qq.com>
2023-12-25 15:09:30 +0800

    uniapp-location

---
+++ pages/index/test9/test9.vue
@@ -0,0 +1,78 @@
+<template>
+	<view>
+		<text>
+			<h1>{{longitude}}</h1>
+			<h1>{{latitude}}</h1>
+		</text>
+		<button type="primary" @click="button1">监听实时地理位置变化事件</button>
+		<button type="primary" @click="button2">关闭监听实时位置变化</button>
+		<button type="primary" @click="button3">监听实时位置失败事件</button>
+		<button type="primary" @click="button4">关闭监听实时位置失败事件</button>
+		<button type="primary" @click="button5">开启小程序进入前台时接收位置消息</button>
+		<button type="primary" @click="button6">关闭小程序进入前台时接收位置消息</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				longitude: 0,
+				latitude: 0,
+			}
+		},
+		methods: {
+			button1() {
+				uni.onLocationChange((res) => {
+					console.log('纬度：' + res.latitude);
+					console.log('经度：' + res.longitude);
+					this.longitude = res.longitude;
+					this.latitude = res.latitude;
+				})
+				console.log("设置完成");
+			},
+			button2() {
+				uni.offLocationChange((res) => {
+					console.log("关闭完成");
+				})
+
+			},
+			button3() {
+				uni.onLocationChangeError((res) => {
+					console.log("失败：", res.errMsg);
+				})
+			},
+			button4() {
+				uni.offLocationChangeError(() => {
+					console.log("关闭");
+				})
+			},
+			button5() {
+				uni.startLocationUpdate({
+					success: () => {
+						console.log('开启小程序接收位置消息成功')
+					},
+					fail: (err) => {
+						console.log('开启小程序接收位置消息失败',err)
+					}
+				})
+			},
+			button6() {
+				uni.stopLocationUpdate({
+					success: () => {
+						console.log('关闭小程序接收位置消息成功')
+					},
+					fail: (err) => {
+						console.log('关闭小程序接收位置消息失败',err)
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	button {
+		margin: 5px;
+	}
+</style>
